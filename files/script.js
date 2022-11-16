24c21e544df9eb129ee4859332b5a0c1949d97a2
Peter Lee <PeterLee2094@gmail.com>
2022-11-16 13:26:54 -0600

    added instructions to document

--- files/script.js
+++ files/script.js
@@ -3,3 +3,27 @@ $(onReady);
 function onReady(){
     console.log('hello world');
 }
+
+/* 
+Instructions:
+- input 
+    - first name
+    - last name 
+    - ID number
+    - job title
+    - annual salary
+
+- submit 
+    - collect form information
+    - calculate monthly cost
+        - show on DOM
+    - clear inputs 
+
+    ## restriction ##
+        - if monthly cost exceeds 20,000 
+                - make background red
+
+- add remove button 
+    - remove employ from list 
+    
+*/

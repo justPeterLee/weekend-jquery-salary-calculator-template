390c2756d60f1a0fa362cdb8dd7aa2e0cff553d1
Peter Lee <PeterLee2094@gmail.com>
2022-11-17 14:44:09 -0600

    added a funtion to collect all of inputs calues checks if values are valid appends them to an object and then to an array

--- files/script.js
+++ files/script.js
@@ -1,9 +1,3 @@
-$(onReady);
-
-function onReady(){
-    console.log('hello world');
-}
-
 /* 
 Instructions:
 - input 
@@ -27,3 +21,53 @@ Instructions:
     - remove employ from list 
     
 */
+
+$(onReady);
+
+function onReady(){
+    console.log('hello world');
+    $('#submit-btn').on('click', getInputs);
+}
+
+let employeeData = [];
+function getInputs(){
+    // prevents page from reloading
+    event.preventDefault();
+
+    // creates an array of all the input values
+    let inputs = {};
+
+    let fname = $("#fname").val();      // index 0
+    let lname = $("#lname").val();      // index 1
+    let employID = $("#ID").val();      // index 2
+    let jobTitle = $("#job").val();     // index 3
+    let salary = $("#salary").val();    // index 4
+    
+    inputs.push(fname, lname, employID, jobTitle, salary);  //[fname, lname, employID, jonTitle, salary]
+
+    // checks if all input forms are valid
+    if(validateForm(inputs)){
+        console.log('sending data');
+    }
+    else{
+        alert('inputs not good')
+    }
+
+    $('.employee-input').val('')
+}
+
+function validateForm(array){
+    for(values of array){
+        if(values.replace(/\s/g, "") === ''){
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function render(){
+
+}
+
+function calcMonthly(){}
